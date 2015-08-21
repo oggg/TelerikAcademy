72dec92e50e322a141afcd3112c5b7404e4b1601
oggg <ogggster@gmail.com>
2015-08-21 15:19:36 +0300

    Refactoring

---
+++ High-Quality-Code/13. Refactoring/Matrix/MatrixCell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class MatrixCell
+    {
+        private static int MIN_LENGTH = 1;
+        private static int MAX_LENGTH = 100;
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public MatrixCell(int x, int y)
+        {
+            this.Row = x;
+            this.Column = y;
+        }
+
+        public static MatrixCell operator +(MatrixCell firstCell, MatrixCell secondCell)
+        {
+            return new MatrixCell(firstCell.Row + secondCell.Row, firstCell.Column + secondCell.Column);
+        }
+    }
+}
