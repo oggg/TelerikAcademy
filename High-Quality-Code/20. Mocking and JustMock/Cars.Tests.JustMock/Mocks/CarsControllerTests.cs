9644ff5cc38a57b1d0b755e716a08b5af22cd07b
oggg <ogggster@gmail.com>
2015-10-15 21:13:06 +0300

    Mocking

---
+++ High-Quality-Code/20. Mocking and JustMock/Cars.Tests.JustMock/Mocks/CarsControllerTests.cs
@@ -0,0 +1,131 @@
+﻿namespace Cars.Tests.JustMock
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Cars.Contracts;
+    using Cars.Controllers;
+    using Cars.Models;
+    using Cars.Tests.JustMock.Mocks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+    [TestClass]
+    public class CarsControllerTests
+    {
+        private ICarsRepository carsData;
+        private CarsController controller;
+
+        public CarsControllerTests()
+            : this(new JustMockCarsRepository())
+        {
+        }
+
+        public CarsControllerTests(ICarsRepositoryMock carsDataMock)
+        {
+            this.carsData = carsDataMock.CarsData;
+        }
+
+        [TestInitialize]
+        public void CreateController()
+        {
+            this.controller = new CarsController(this.carsData);
+        }
+
+        [TestMethod]
+        public void IndexShouldReturnAllCars()
+        {
+            var model = (ICollection<Car>)this.GetModel(() => this.controller.Index());
+
+            Assert.AreEqual(4, model.Count);
+        }
+
+        [TestMethod]
+        public void SortCarsByMake()
+        {
+            var sortedByMake = (ICollection<Car>)this.GetModel(() => this.controller.Sort("make"));
+            var firstCarMake = sortedByMake.FirstOrDefault().Make;
+
+            Assert.AreEqual("Audi", firstCarMake);
+        }
+
+        [TestMethod]
+        public void SortCarsByYear()
+        {
+            var sortedByMake = (ICollection<Car>)this.GetModel(() => this.controller.Sort("year"));
+            var firstCarYear = sortedByMake.FirstOrDefault().Year;
+
+            Assert.AreEqual(2005, firstCarYear);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortCarsByCondition()
+        {
+            var sortedByMake = (ICollection<Car>)this.GetModel(() => this.controller.Sort("model"));
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarIsNull()
+        {
+            var model = (Car)this.GetModel(() => this.controller.Add(null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarMakeIsNull()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "",
+                Model = "330d",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingCarShouldThrowArgumentNullExceptionIfCarModelIsNull()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "BMW",
+                Model = "",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+        }
+
+        [TestMethod]
+        public void AddingCarShouldReturnADetail()
+        {
+            var car = new Car
+            {
+                Id = 15,
+                Make = "BMW",
+                Model = "330d",
+                Year = 2014
+            };
+
+            var model = (Car)this.GetModel(() => this.controller.Add(car));
+
+            Assert.AreEqual(1, model.Id);
+            Assert.AreEqual("Audi", model.Make);
+            Assert.AreEqual("A4", model.Model);
+            Assert.AreEqual(2005, model.Year);
+        }
+
+        private object GetModel(Func<IView> funcView)
+        {
+            var view = funcView();
+            return view.Model;
+        }
+    }
+}
---
+++ High-Quality-Code/20. Mocking and JustMock/Cars/Contracts/ICarsRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Cars.Contracts
+{
+    using System.Collections.Generic;
+    using Cars.Models;
+
+    public interface ICarsRepository
+    {
+        int TotalCars { get; }
+
+        void Add(Car car); // tested
+
+        void Remove(Car car);
+
+        Car GetById(int id); //tested
+
+        ICollection<Car> All(); //tested
+
+        ICollection<Car> SortedByMake(); //tested
+
+        ICollection<Car> SortedByYear(); //tested
+
+        ICollection<Car> Search(string condition);
+    }
+}
